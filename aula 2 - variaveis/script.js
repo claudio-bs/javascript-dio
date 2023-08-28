1c76e8e882a9e9bb9c332f3e285fdb4b9d6bfd86
claudio-bs <claudio_bs@icloud.com>
2023-08-28 10:57:51 -0300

    Create script.js

---
+++ aula 2 - variaveis/script.js
@@ -0,0 +1,18 @@
+// tipos primitivos
+
+//boolean
+var vOuF = false;
+console.log(typeof(vOuF));
+
+//number
+var numeroqualquer = 1;
+console.log(typeof(numeroqualquer));
+
+//string
+var nome = 'Claudio'
+console.log(typeof(nome));
+
+//como declarar
+var variavel = 'Claudio';
+variavel = 'Santana';
+console.log(variavel);
