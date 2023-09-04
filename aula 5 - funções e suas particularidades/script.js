ee36c891037f60171f1cc66933db76d2d01d2af4
claudio-bs <claudio_bs@icloud.com>
2023-09-04 09:19:41 -0300

    Create script.js

---
+++ aula 5 - funções e suas particularidades/script.js
@@ -0,0 +1,15 @@
+/*
+
+function funcao() {
+    console.log('Essa mensagem veio de uma função.')
+}
+
+funcao();
+
+*/
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+
+mensagem('Tudo certo', 'jovem!');
